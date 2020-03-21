ff7f7bc8fb66e760d660d38073be6ee39c28c020
Dannielle Taylor <dannielletaylor@outlook.com>
2020-03-21 18:07:51 -0500

    loading server.js file

---
+++ server.js
@@ -0,0 +1,28 @@
+//Dependencies
+var express = require("express");
+var bodyParser= require("body-parser");
+var path = require("path");
+
+//Set up Express App
+var app = express();
+var PORT = process.env.PORT;
+
+//express middleware for static files
+app.use(express.static("app/public"));
+
+//Sets up the Express app to handle data parsing and formatting
+app.use(bodyParser.json());
+app.use(bodyParse.urlencoded({ extended: true}));
+app.use(bodyParser.text({type: "text/html"}));
+app.use(bodyParse.json({ type: "application/vnd.api+json"}));
+
+//Routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+//Listener
+app.listen(PORT, function() {
+    console.log("app listening on port: ", PORT);
+});
+
+
