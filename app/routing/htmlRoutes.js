911f546349e64a85032cc1a3b4edda097e1d32da
Dannielle Taylor <dannielletaylor@outlook.com>
2020-03-21 19:02:51 -0500

    adding htmlRoutes file

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+
+//Require path to parse directory structures
+var path = require("path");
+
+//Defining Routes that will be exported to the server
+function htmlRoutes(app) {
+
+    //GET route to /survey which should display the survey page
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(_dirname + "../public/survey.html"));
+    });
+
+    //Default to route any other url to home
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
+
+//Export for use in main file
+module.exports = htmlRoutes;
