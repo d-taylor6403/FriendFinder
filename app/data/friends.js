81f110cbc6dc5c4d80a379c371e1ae442846de4d
Dannielle Taylor <dannielletaylor@outlook.com>
2020-03-21 19:59:28 -0500

    adding seed data for friendsData file

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+//Friends Data Seed
+var friendsData = [
+    {
+        name: "Rebecca Thomas",
+        photo: "https://www.pexels.com/photo/photo-of-a-woman-3115030/",
+        score: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    }
+];
+
+//Export to API Route
+module.exports = friendsData;
